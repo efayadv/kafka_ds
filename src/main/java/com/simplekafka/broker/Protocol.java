a4cf6697839713ad0fb41f791e8babed12cb5641
emiliofayad <emiliofav@gmail.com>
2026-08-29 21:35:51 -0500

    produce request protocol

--- src/main/java/com/simplekafka/broker/Protocol.java
+++ src/main/java/com/simplekafka/broker/Protocol.java
@@ -1,5 +1,44 @@
 package main.java.com.simplekafka.broker;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 public class Protocol {
-    
+    // Client request types
+    public static final byte PRODUCE = 0x01;
+    public static final byte FETCH = 0x02;
+    public static final byte METADATA = 0x03;
+    public static final byte CREATE_TOPIC = 0x04;
+
+    // Broker response types
+    public static final byte PRODUCE_RESPONSE = 0x11;
+    public static final byte FETCH_RESPONSE = 0x12;
+    public static final byte METADATA_RESPONSE = 0x13;
+    public static final byte CREATE_TOPIC_RESPONSE = 0x14;
+
+    public static ByteBuffer encodeProduceRequest(String topic, int partition, byte[] message) {
+        byte[] topicBytes = topic.getBytes(StandardCharsets.UTF_8);
+
+        int size = 
+            1 //request type
+            + 2 //topic string length
+            + topicBytes.length //N bytes of topic name
+            + 4 //partition
+            + 4 // message length
+            + message.length;
+
+        ByteBuffer buffer = ByteBuffer.allocate(size); // allocating size for our produce
+
+        buffer.put(PRODUCE); //Type
+        buffer.putShort((short) topicBytes.length);
+        buffer.put(topicBytes);
+        buffer.putInt(partition);
+        buffer.putInt(message.length);
+        buffer.put(message);
+
+        buffer.flip();
+        return buffer;
+
+    }
+
 }
