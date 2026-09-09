5841d973f4d1b973f0afcf888f93647b4714912c
emiliofayad <emiliofav@gmail.com>
2026-09-09 17:25:51 -0500

    TODO: send & fetch

--- src/main/java/com/simplekafka/client/SimpleKafkaClient.java
+++ src/main/java/com/simplekafka/client/SimpleKafkaClient.java
@@ -2,6 +2,7 @@ package com.simplekafka.client;
 
 import com.simplekafka.broker.BrokerInfo;
 import com.simplekafka.broker.Protocol;
+import com.simplekafka.broker.SimpleKafkaBroker;
 import com.simplekafka.broker.Protocol.PartitionMetadata;
 import com.simplekafka.broker.Protocol.TopicMetadata;
 
@@ -72,20 +73,75 @@ public class SimpleKafkaClient {
             //update topic metadata, conchashmap <String, Topic Metadata>
             topicMetadata.clear();
             for (Protocol.TopicMetadata topic : result.getTopics()) {
-                List<PartitionMetadata> partitions = new ArrayList<>()
+                List<PartitionInfo> partitions = new ArrayList<>();
 
-                topicMetadata.put(topic.getName(), topic);
+                for (Protocol.PartitionMetadata partition : topic.getPartitions()) {
+                    PartitionInfo p = new PartitionInfo(partition.getId(), partition.getLeader(), partition.getReplicas());
+                    partitions.add(p);
+                }
+
+                topicMetadata.put(topic.getName(), new TopicMetadata(topic.getName(), partitions));
             }
-            
 
+            LOGGER.info("Metadata refreshed: " + brokers.size() + " brokers, " + 
+                       topicMetadata.size() + " topics");
+        } 
+    }
 
-        } catch () {
-
+    public boolean createTopic(String topic, int numPartitions, short replicationFactor) throws IOException {
+        if (brokers.isEmpty()) {
+            refreshMetadata();
+            if (brokers.isEmpty()) {
+                throw new IOException("No brokers available");
+            }
         }
 
+        BrokerInfo broker = brokers.values().iterator().next();
+
+        try (SocketChannel channel = SocketChannel.open()) {
+            channel.connect(new InetSocketAddress(broker.getHost(), broker.getPort()));
+
+            // send create topic request
+            ByteBuffer request = Protocol.encodeCreateTopicRequest(topic, numPartitions, replicationFactor);
+            channel.write(request);
+
+            //read response
+            ByteBuffer response = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE);
+            int bytesRead = channel.read(response);
+            if (bytesRead < 0) {
+                throw new IOException("No data from broker");
+            }
+
+            response.flip();
+            
+            byte responseType = response.get();
+            if (responseType != Protocol.CREATE_TOPIC_RESPONSE){
+                if (responseType == Protocol.ERROR_RESPONSE) {
+                    short errorLength = response.getShort();
+                    byte[] errorBytes = new byte[errorLength];
+                    response.get(errorBytes);
+                    String error = new String(errorBytes);
+                    LOGGER.warning("Error creating topic: " + topic);
+                    return false;
+                }
+                throw new IOException("Invalid create topic response type: " + responseType);
+            }
+
+            byte status = response.get();
+            boolean success = status == 0;
+
+            if (success) {
+                //refresh metadata to include new topic
+                refreshMetadata();
+            }
+
+            return success; 
+        }  
     }
+
     public long send(String topic, int partition, byte[] message) throws IOException {
         // Send a message to a specific topic-partition
+        
         return -1; // Placeholder
     }
     public List<byte[]> fetch(String topic, int partition, long offset, int maxBytes) throws IOException {
@@ -93,4 +149,56 @@ public class SimpleKafkaClient {
         return new ArrayList<>(); // Placeholder
     }
     // Additional methods and inner classes for metadata
+
+    public static class TopicMetadata {
+        private final String name;
+        private final List<PartitionInfo> partitions;
+        
+        public TopicMetadata(String name, List<PartitionInfo> partitions) {
+            this.name = name;
+            this.partitions = partitions;
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public List<PartitionInfo> getPartitions() {
+            return new ArrayList<>(partitions);
+        }
+        
+        @Override
+        public String toString() {
+            return "TopicMetadata{name='" + name + "', partitions=" + partitions + "}";
+        }
+    }
+
+    public static class PartitionInfo {
+        private final int id;
+        private final int leader;
+        private final List<Integer> followers;
+        
+        public PartitionInfo(int id, int leader, List<Integer> followers) {
+            this.id = id;
+            this.leader = leader;
+            this.followers = followers;
+        }
+        
+        public int getId() {
+            return id;
+        }
+        
+        public int getLeader() {
+            return leader;
+        }
+        
+        public List<Integer> getFollowers() {
+            return new ArrayList<>(followers);
+        }
+        
+        @Override
+        public String toString() {
+            return "PartitionInfo{id=" + id + ", leader=" + leader + ", followers=" + followers + "}";
+        }
+    }
 }
